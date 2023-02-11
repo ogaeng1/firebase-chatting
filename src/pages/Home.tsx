c14df80769e4d0a2c14b1c8183e93eede7d5f6e8
ogaeng1 <rudals2148@naver.com>
2023-02-11 23:30:55 +0900

    [new] 초기 세팅

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return <div className="w-full h-10 bg-blue-500">홈</div>
+}
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+        <span>로그인</span>
+    );
+}
+
+export default Login;
