56b05445d8ebd5496f9073b4a0d660aab6f08b21
ogaeng1 <rudals2148@naver.com>
2023-02-12 21:57:26 +0900

    [fix] 파이어베이스 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/tailwind.css';
 import App from './App';
-import { auth } from "./firebase";
-
-console.log(auth);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
