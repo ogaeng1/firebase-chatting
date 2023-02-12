a1e482a0c43940f7f6f50028d49d0b645eb80c7d
ogaeng1 <rudals2148@naver.com>
2023-02-12 21:55:15 +0900

    [fix] ui 수정 중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { auth } from "../firebase";
+console.log(auth);
 
 const Home = () => {
-    return <div className="w-full h-10 bg-blue-500">홈</div>
+    return (
+        <div className="w-full h-10 bg-blue-500">
+            {auth.currentUser?.displayName + `님 안녕하세요`}
+        </div>
+    );
 }
 
 export default Home;
