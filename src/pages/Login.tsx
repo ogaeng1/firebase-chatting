1a3582cfc7e77161fc98cb53bffc825cc80a52d0
ogaeng1 <rudals2148@naver.com>
2023-02-12 21:54:52 +0900

    [fix] 구글 로그인 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+
+
 const Login = () => {
-    return (
-        <span>로그인</span>
-    );
+    const [google, setGoogle] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    auth.languageCode = 'it';
+
+    if (google === true) {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential?.accessToken;
+            const user = result.user;
+            if (user !== null) {
+                navigate("/home");
+            }
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.customData.email;
+            const credential = GoogleAuthProvider.credentialFromError(error);
+        });
+    }
+    
+  return (
+    <div onClick={() => setGoogle(true)} className="w-[150px] h-[25px] flex cursor-pointer border border-blue-400">
+        <img src="/images/google.png" alt='' />
+        <span>구글로 시작하기</span>
+    </div>
+  );
 }
 
 export default Login;
