c3e5514bc26118d322cc12a573cd4c533a8015c8
ogaeng1 <rudals2148@naver.com>
2023-02-13 23:27:10 +0900

    [fix] url 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Layout from "./components/Layout";
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Layout>
         </BrowserRouter>
     );
 }
