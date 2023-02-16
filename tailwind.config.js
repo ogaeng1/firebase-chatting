4bb3ec29f6dc663fdc3bd6ecd127790656442131
ogaeng1 <rudals2148@naver.com>
2023-02-16 17:37:05 +0900

    [fix] 커스텀

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        overlay: "rgba(0, 0, 0, 0.4)",
+      },
+    },
   },
   plugins: [],
 };
