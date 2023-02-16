5811c27cc9bd68ce68b6d942a48808ced7dec9b4
ogaeng1 <rudals2148@naver.com>
2023-02-16 17:38:11 +0900

    [fix] 회원가입 페이지 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Layout from "../components/Layout";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = () => {
     const [emailValue, setEmailValue] = useState<string>("");
     const [passWord, setPassWord] = useState<string>("");
+    const navigate = useNavigate();
 
     const auth = getAuth();
-    createUserWithEmailAndPassword(auth, emailValue, passWord)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        alert("회원가입완료")
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-    });
-
     const onSubmit = (e: any) => {
-        e.preventDeafault();
+        e.preventDefault();
+        createUserWithEmailAndPassword(auth, emailValue, passWord)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            alert("회원가입완료");
+            navigate("/login")
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
     }
 
     return (
         <Layout>
             <form onSubmit={onSubmit} className="w-[20%] h-1/2 flex flex-col justify-center gap-2">
                 <input type="email" required placeholder="이메일을 입력하세요" className="w-full h-[40px] border border-gray-800 rounded-[10px] px-[5px]" onChange={(e) => {setEmailValue(e.target.value)}} />
-                <input type="password" required placeholder="비밀번호를 입력하세요" className="w-full h-[40px] border border-gray-800 rounded-[10px] px-[5px]" onChange={(e) => {setPassWord(e.target.value)}} />
-                <button onClick={() => createUserWithEmailAndPassword} className="w-full h-[40px] bg-gray-900 text-white">회원가입</button>
+                <input type="password" required placeholder="비밀번호 (6자리 이상)" className="w-full h-[40px] border border-gray-800 rounded-[10px] px-[5px]" onChange={(e) => {setPassWord(e.target.value)}} />
+                <button type="submit" className="w-full h-[40px] bg-gray-900 text-white">회원가입</button>
             </form>
         </Layout>
     );
