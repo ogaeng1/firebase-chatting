64923a995208ebaec3769bd44964c95c8c61ff9a
OH <rudals2148@naver.com>
2023-02-21 02:06:10 +0900

    [fix] 반응형 ui 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import ChatButton from "../components/ChatButton";
 import Header from "../components/Header";
-import MainBanner from "../components/home/MainBanner";
 import Layout from "../components/Layout";
+import Clock from "../components/Clock";
+import ChattingRoom from "../components/ChattingRoom";
 
 const Home = () => {
     return (
         <Layout>
-            <div className="w-full h-full flex flex-col">
-                <Header />
-                <MainBanner />
-                <ChatButton />
+            <Header />
+            <div className="w-full h-full flex flex-col items-center mdSize-mainScreen">
+                <div className="w-full md:w-1/2">
+                    <Clock />
+                </div>
+                <div className="w-[90%] h-[80vh] bg-[aliceblue] rounded-lg md:w-1/2 md:h-[90vh]">
+                    <ChattingRoom />
+                </div>
             </div>
         </Layout>
     );
