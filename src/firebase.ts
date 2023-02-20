568b88de6fa5c0c5e973601cd02d4336b61c78d6
OH <rudals2148@naver.com>
2023-02-21 02:04:07 +0900

    [fix] api 키 -> env로 변경

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBPjMffw4gnfA911Dqn2WlPrtPl_PEMqEY",
-  authDomain: "side-project-aca2a.firebaseapp.com",
-  projectId: "side-project-aca2a",
-  storageBucket: "side-project-aca2a.appspot.com",
-  messagingSenderId: "865263554448",
-  appId: "1:865263554448:web:177c13255bbf42aaa9ed15"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { auth, db };
