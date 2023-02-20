9bfa1ccb8f426b015ee7d30857207c47d6a9eb61
OH <rudals2148@naver.com>
2023-02-21 02:03:35 +0900

    [fix] 코드 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Layout from "./components/Layout";
-import SignUp from "./pages/SignUp";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Layout>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
